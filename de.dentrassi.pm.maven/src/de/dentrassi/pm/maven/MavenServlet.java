1c4d3faa4301096909487a088de3b490eebf071f
Jens Reimann <jens.reimann@ibh-systems.com>
2014-11-25 11:31:25 +0100

    add license header

--- de.dentrassi.pm.maven/src/de/dentrassi/pm/maven/MavenServlet.java
+++ de.dentrassi.pm.maven/src/de/dentrassi/pm/maven/MavenServlet.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Jens Reimann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jens Reimann - initial API and implementation
+ *******************************************************************************/
 package de.dentrassi.pm.maven;
 
 import java.io.IOException;
@@ -20,6 +30,7 @@ import org.w3c.dom.Node;
 
 import com.google.common.io.ByteStreams;
 
+import de.dentrassi.pm.common.XmlHelper;
 import de.dentrassi.pm.storage.service.Artifact;
 import de.dentrassi.pm.storage.service.MetaKey;
 import de.dentrassi.pm.storage.service.StorageService;
--- de.dentrassi.pm.meta/src/de/dentrassi/pm/meta/ChannelAspect.java
+++ de.dentrassi.pm.meta/src/de/dentrassi/pm/meta/ChannelAspect.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Jens Reimann.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Jens Reimann - initial API and implementation
+ *******************************************************************************/
 package de.dentrassi.pm.meta;
 
 import de.dentrassi.pm.meta.extract.Extractor;
@@ -11,7 +21,7 @@ public interface ChannelAspect
 
     /**
      * Get the factory id
-     * 
+     *
      * @return the factory id
      */
     public String getId ();
