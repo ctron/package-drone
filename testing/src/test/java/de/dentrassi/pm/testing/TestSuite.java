07ba7b1165cc338955bebcc0445ca5a128c0604c
Jens Reimann <jens.reimann@ibh-systems.com>
2015-02-05 13:39:45 +0100

    preparing to test with open sauce

--- testing/src/test/java/de/dentrassi/pm/testing/TestSuite.java
+++ testing/src/test/java/de/dentrassi/pm/testing/TestSuite.java
@@ -11,7 +11,9 @@
 package de.dentrassi.pm.testing;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.ServerSocket;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.AfterClass;
@@ -20,12 +22,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 @RunWith ( Suite.class )
 @SuiteClasses ( { BasicTest.class, SetupTest.class, DefaultTest.class, MailTest.class, UploadTest.class } )
 public class TestSuite
 {
+    private static String SAUCE_USER_NAME = System.getProperty ( "sauce.username" );
+
+    private static String SAUCE_ACCESS_KEY = System.getProperty ( "sauce.accessKey" );
 
     private static RemoteWebDriver driver;
 
@@ -35,15 +42,39 @@ public class TestSuite
     }
 
     @BeforeClass
-    public static void setupBrowser ()
+    public static void setupBrowser () throws MalformedURLException
     {
-        driver = new FirefoxDriver ();
+        if ( hasSauce () )
+        {
+            driver = createSauce ( "Windows 8.1", "chrome", null );
+        }
+        else
+        {
+            driver = new FirefoxDriver ();
+        }
+    }
+
+    private static boolean hasSauce ()
+    {
+        return SAUCE_ACCESS_KEY != null && SAUCE_USER_NAME != null && !SAUCE_ACCESS_KEY.isEmpty () && !SAUCE_USER_NAME.isEmpty ();
+    }
+
+    protected static RemoteWebDriver createSauce ( final String os, final String browser, final String version ) throws MalformedURLException
+    {
+        final DesiredCapabilities capabilities = new DesiredCapabilities ();
+        capabilities.setCapability ( CapabilityType.BROWSER_NAME, browser );
+        if ( version != null )
+        {
+            capabilities.setCapability ( CapabilityType.VERSION, version );
+        }
+        capabilities.setCapability ( CapabilityType.PLATFORM, os );
+        return new RemoteWebDriver ( new URL ( "http://" + SAUCE_USER_NAME + ":" + SAUCE_ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub" ), capabilities );
     }
 
     @AfterClass
     public static void destroyBrowser ()
     {
-        driver.close ();
+        driver.quit ();
     }
 
     private static Process PROCESS;
