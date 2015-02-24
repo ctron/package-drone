b713023c2ef04882fb1dcabc957dada357af9f53
Jens Reimann <jens.reimann@ibh-systems.com>
2015-02-24 10:55:57 +0100

    clean up license headers

--- de.dentrassi.pm.sec.service.core/src/de/dentrassi/pm/sec/service/core/CorePasswordChecker.java
+++ de.dentrassi.pm.sec.service.core/src/de/dentrassi/pm/sec/service/core/CorePasswordChecker.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2015 Jens Reimann.
+ * Copyright (c) 2015 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Jens Reimann - initial API and implementation
+ *     IBH SYSTEMS GmbH - initial API and implementation
  *******************************************************************************/
 package de.dentrassi.pm.sec.service.core;
 
