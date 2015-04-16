ad1c2be810f0a46884f07fc4f7f23eff4f6287d3
Jens Reimann <jens.reimann@ibh-systems.com>
2015-04-16 17:33:17 +0200

    Extract the pom file of a jar file, if found, #71

---
+++ de.dentrassi.pm.maven/src/de/dentrassi/pm/maven/internal/MavenPomExtractorFactory.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
+package de.dentrassi.pm.maven.internal;
+
+import de.dentrassi.pm.aspect.ChannelAspect;
+import de.dentrassi.pm.aspect.ChannelAspectFactory;
+import de.dentrassi.pm.aspect.virtual.Virtualizer;
+
+public class MavenPomExtractorFactory implements ChannelAspectFactory
+{
+    public static final String ID = "maven.extractor";
+
+    @Override
+    public ChannelAspect createAspect ()
+    {
+        return new ChannelAspect () {
+
+            @Override
+            public String getId ()
+            {
+                return ID;
+            }
+
+            @Override
+            public Virtualizer getArtifactVirtualizer ()
+            {
+                return new MavenPomExtractor ();
+            }
+        };
+    }
+
+}
