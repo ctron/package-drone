4ef3a56c095e53c5e52a3d8cb54f5622f8e8f3ba
Jens Reimann <jens.reimann@ibh-systems.com>
2015-04-20 10:18:35 +0200

    create a maven directory structure for extracted POM files, #71
    This commit adds an artifact to the maven repository structure if
    there is a child-artifact element which is an extracted POM file.
    
    It takes GAV from the POM file and the extension is the extension of
    the artifact name.
    
    This is the last commit which enables uploading JAR files, which
    contain a "pom" and re-created the maven repository structure, allowing
    maven read access to this artifact.

--- de.dentrassi.pm.maven/src/de/dentrassi/pm/maven/internal/MavenRepositoryChannelAggregator.java
+++ de.dentrassi.pm.maven/src/de/dentrassi/pm/maven/internal/MavenRepositoryChannelAggregator.java
@@ -33,13 +33,15 @@ public class MavenRepositoryChannelAggregator implements ChannelAggregator
     @Override
     public Map<String, String> aggregateMetaData ( final AggregationContext context ) throws Exception
     {
+        final Map<String, ArtifactInformation> map = makeMap ( context );
+
         final Map<String, String> result = new HashMap<> ();
 
         final ChannelData cs = new ChannelData ();
 
         for ( final ArtifactInformation art : context.getArtifacts () )
         {
-            final MavenInformation info = makeInfo ( art );
+            final MavenInformation info = makeInfo ( art, map );
             if ( info != null )
             {
                 // add
@@ -56,9 +58,22 @@ public class MavenRepositoryChannelAggregator implements ChannelAggregator
         return result;
     }
 
-    private MavenInformation makeInfo ( final ArtifactInformation art )
+    private Map<String, ArtifactInformation> makeMap ( final AggregationContext context )
+    {
+        final Map<String, ArtifactInformation> result = new HashMap<> ();
+
+        for ( final ArtifactInformation art : context.getArtifacts () )
+        {
+            result.put ( art.getId (), art );
+        }
+
+        return result;
+    }
+
+    private MavenInformation makeInfo ( final ArtifactInformation art, final Map<String, ArtifactInformation> map )
     {
         final MavenInformation info = new MavenInformation ();
+
         try
         {
             MetaKeys.bind ( info, art.getMetaData () );
@@ -70,8 +85,64 @@ public class MavenRepositoryChannelAggregator implements ChannelAggregator
 
         if ( info.getGroupId () != null & info.getArtifactId () != null && info.getVersion () != null )
         {
+            // found direct meta data
             return info;
         }
+
+        final ArtifactInformation pomArt = findChildPom ( art, map );
+        if ( pomArt != null )
+        {
+            try
+            {
+                MetaKeys.bind ( info, pomArt.getMetaData () );
+            }
+            catch ( final Exception e )
+            {
+                return null;
+            }
+
+            if ( info.getGroupId () != null & info.getArtifactId () != null && info.getVersion () != null )
+            {
+                // found pom meta data
+                final String ext = makeExtension ( art.getName () );
+                if ( ext != null )
+                {
+                    info.setExtension ( ext );
+                    return info;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static String makeExtension ( final String name )
+    {
+        final int idx = name.lastIndexOf ( '.' );
+        if ( idx < 0 )
+        {
+            return null;
+        }
+        return name.substring ( idx + 1 );
+    }
+
+    private ArtifactInformation findChildPom ( final ArtifactInformation art, final Map<String, ArtifactInformation> map )
+    {
+        for ( final String childId : art.getChildIds () )
+        {
+            final ArtifactInformation child = map.get ( childId );
+            if ( child == null )
+            {
+                continue;
+            }
+            if ( !child.getName ().equals ( "pom.xml" ) )
+            {
+                continue;
+            }
+
+            return child;
+        }
+
         return null;
     }
 
