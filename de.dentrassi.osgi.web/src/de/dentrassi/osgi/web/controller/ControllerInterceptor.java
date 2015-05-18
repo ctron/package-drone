d012a99e82c100b98f754d3c574b854cf0982ada
Jens Reimann <jens.reimann@ibh-systems.com>
2015-05-18 12:25:41 +0200

    add missing license headers

--- de.dentrassi.osgi.web/src/de/dentrassi/osgi/web/controller/ControllerInterceptor.java
+++ de.dentrassi.osgi.web/src/de/dentrassi/osgi/web/controller/ControllerInterceptor.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2014, 2015 IBH SYSTEMS GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
 package de.dentrassi.osgi.web.controller;
 
 import java.lang.annotation.ElementType;
@@ -11,5 +21,5 @@ import java.lang.annotation.Target;
 @Repeatable ( ControllerInterceptors.class )
 public @interface ControllerInterceptor
 {
-    public Class<? extends ControllerInterceptorProcessor> value ();
+    public Class<? extends ControllerInterceptorProcessor>value ();
 }
