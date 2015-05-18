d1b2133099d340b8e81f734db0185a291b62436a
Jens Reimann <jens.reimann@ibh-systems.com>
2015-05-18 09:29:03 +0200

    fix the Exception handling when a cache entry was not found, closing #85

--- de.dentrassi.pm.storage.service.jpa/src/de/dentrassi/pm/storage/service/jpa/StorageServiceImpl.java
+++ de.dentrassi.pm.storage.service.jpa/src/de/dentrassi/pm/storage/service/jpa/StorageServiceImpl.java
@@ -41,6 +41,7 @@ import javax.persistence.criteria.JoinType;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.eclipse.scada.utils.ExceptionHelper;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
@@ -807,9 +808,26 @@ public class StorageServiceImpl extends AbstractJpaServiceImpl implements Storag
         } );
     }
 
-    public void streamCacheEntry ( final String channelId, final String namespace, final String key, final ThrowingConsumer<CacheEntry> consumer )
+    public void streamCacheEntry ( final String channelId, final String namespace, final String key, final ThrowingConsumer<CacheEntry> consumer ) throws FileNotFoundException
     {
-        doWithHandlerVoid ( ( handler ) -> handler.streamCacheEntry ( channelId, namespace, key, consumer ) );
+        try
+        {
+            doWithHandlerVoid ( ( handler ) -> handler.streamCacheEntry ( channelId, namespace, key, consumer ) );
+        }
+        catch ( final Exception e )
+        {
+            /*
+             * In the case we did not find the entry, we should throw a FileNotFoundException.
+             * However this exception is actually wrapped in a layer of RuntimeExceptions, so we have to
+             * unwrap them in this case.
+             */
+            final Throwable cause = ExceptionHelper.getRootCause ( e );
+            if ( cause instanceof FileNotFoundException )
+            {
+                throw (FileNotFoundException)cause;
+            }
+            throw e;
+        }
     }
 
     public ArtifactGuard createArtifactGuard ()
