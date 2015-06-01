9e93c0fc5015d127543f748c1042b0da0567bc36
Jens Reimann <jens.reimann@ibh-systems.com>
2015-06-01 12:19:08 +0200

    use a different P2 download URL template, #100
    When the OSGi version part contains an underscore (_) the
    DownloadHandler cannot split version and id property by using the
    underscore as a delimiter. Thus we are using a different URL pattern.

--- de.dentrassi.pm.p2/src/de/dentrassi/pm/p2/servlet/DownloadHandler.java
+++ de.dentrassi.pm.p2/src/de/dentrassi/pm/p2/servlet/DownloadHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 IBH SYSTEMS GmbH.
+ * Copyright (c) 2014, 2015 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,8 +11,6 @@
 package de.dentrassi.pm.p2.servlet;
 
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.dentrassi.pm.aspect.common.osgi.OsgiAspectFactory;
 import de.dentrassi.pm.aspect.common.osgi.OsgiExtractor;
 import de.dentrassi.pm.common.MetaKey;
 import de.dentrassi.pm.common.servlet.Handler;
@@ -31,17 +30,29 @@ public class DownloadHandler implements Handler
 {
     private final static Logger logger = LoggerFactory.getLogger ( DownloadHandler.class );
 
+    private static final MetaKey KEY_MIME_TYPE = new MetaKey ( "mime", "type" );
+
+    private static final MetaKey KEY_OSGI_CLASSIFIER = new MetaKey ( OsgiAspectFactory.ID, OsgiExtractor.KEY_CLASSIFIER );
+
+    private static final MetaKey KEY_OSGI_ID = new MetaKey ( OsgiAspectFactory.ID, OsgiExtractor.KEY_NAME );
+
+    private static final MetaKey KEY_OSGI_VERSION = new MetaKey ( OsgiAspectFactory.ID, OsgiExtractor.KEY_VERSION );
+
     private final Channel channel;
+
+    private final String id;
+
+    private final String version;
 
     private final String filename;
 
     private final String classifier;
 
-    private static final Pattern PATTERN = Pattern.compile ( "(.*)_(.*?)\\.jar" );
-
-    public DownloadHandler ( final Channel channel, final String filename, final String classifier )
+    public DownloadHandler ( final Channel channel, final String id, final String version, final String filename, final String classifier )
     {
         this.channel = channel;
+        this.id = id;
+        this.version = version;
         this.filename = filename;
         this.classifier = classifier;
     }
@@ -54,50 +65,43 @@ public class DownloadHandler implements Handler
     @Override
     public void process ( final HttpServletRequest req, final HttpServletResponse resp ) throws Exception
     {
-        final Matcher m = PATTERN.matcher ( this.filename );
-        if ( !m.matches () )
-        {
-            logger.warn ( "Invalid syntax: {}", this.filename );
-            resp.setStatus ( HttpServletResponse.SC_NOT_FOUND );
-            return;
-        }
-
-        final String name = m.group ( 1 );
-        final String version = m.group ( 2 );
-        logger.debug ( "Looking for bundle: {}/{}", name, version );
+        logger.debug ( "Looking for bundle: {}/{}", this.id, this.version );
 
         // TODO: speed up search
         for ( final Artifact a : this.channel.getArtifacts () )
         {
             final Map<MetaKey, String> md = a.getInformation ().getMetaData ();
 
-            final String thisClassifier = md.get ( new MetaKey ( "osgi", OsgiExtractor.KEY_CLASSIFIER ) );
-            final String thisName = md.get ( new MetaKey ( "osgi", OsgiExtractor.KEY_NAME ) );
-            final String thisVersion = md.get ( new MetaKey ( "osgi", OsgiExtractor.KEY_VERSION ) );
+            final String thisClassifier = md.get ( KEY_OSGI_CLASSIFIER );
+            final String thisId = md.get ( KEY_OSGI_ID );
+            final String thisVersion = md.get ( KEY_OSGI_VERSION );
 
-            logger.debug ( "This - name: {}, version: {}, classifier: {}", thisName, thisVersion, thisClassifier );
+            logger.debug ( "This - id: {}, version: {}, classifier: {}", thisId, thisVersion, thisClassifier );
 
             if ( thisClassifier == null || !thisClassifier.equals ( this.classifier ) )
             {
                 continue;
             }
 
-            if ( thisName == null || !thisName.equals ( name ) )
+            if ( thisId == null || !thisId.equals ( this.id ) )
             {
                 continue;
             }
 
-            if ( thisVersion == null || !thisVersion.equals ( version ) )
+            if ( thisVersion == null || !thisVersion.equals ( this.version ) )
             {
                 continue;
             }
 
             logger.debug ( "Streaming artifact: {} / {} ", a.getInformation ().getName (), a.getId () );
-            DownloadHelper.streamArtifact ( resp, a, md.get ( new MetaKey ( "mime", "type" ) ), true );
+            DownloadHelper.streamArtifact ( resp, a, md.get ( KEY_MIME_TYPE ), true, art -> this.filename );
             return;
         }
 
-        logger.warn ( "Artifact not found - name: {}, version: {}, classifier: {}", name, version, this.classifier );
+        final String message = String.format ( "Artifact not found - name: %s, version: %s, classifier: %s", this.id, this.version, this.classifier );
+        logger.warn ( message );
         resp.setStatus ( HttpServletResponse.SC_NOT_FOUND );
+        resp.setContentType ( "text/plain" );
+        resp.getWriter ().println ( message );
     }
 }
