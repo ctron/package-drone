a94f05dd63d380b40626639293c7826acca256d7
Jens Reimann <jens.reimann@ibh-systems.com>
2015-11-09 09:31:11 +0100

    add missing license header

--- bundles/org.eclipse.packagedrone.repo.web/src/org/eclipse/packagedrone/repo/web/utils/Channels.java
+++ bundles/org.eclipse.packagedrone.repo.web/src/org/eclipse/packagedrone/repo/web/utils/Channels.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBH SYSTEMS GmbH - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.packagedrone.repo.web.utils;
 
 import static com.google.common.net.UrlEscapers.urlPathSegmentEscaper;
@@ -8,9 +18,9 @@ import org.eclipse.packagedrone.repo.channel.ChannelArtifactInformation;
 import org.eclipse.packagedrone.repo.channel.ChannelId;
 import org.eclipse.packagedrone.repo.channel.ChannelNotFoundException;
 import org.eclipse.packagedrone.repo.channel.ChannelService;
-import org.eclipse.packagedrone.repo.channel.ReadableChannel;
 import org.eclipse.packagedrone.repo.channel.ChannelService.By;
 import org.eclipse.packagedrone.repo.channel.ChannelService.ChannelOperation;
+import org.eclipse.packagedrone.repo.channel.ReadableChannel;
 import org.eclipse.packagedrone.web.ModelAndView;
 import org.eclipse.packagedrone.web.common.CommonController;
 
